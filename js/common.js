4feeeb0538bbae31d69e5694e84f4f4ef706193f
NickZmh <nickzmh92@gmail.com>
2022-06-22 23:15:06 +0200

    implement the how it works wit mobile version

--- js/common.js
+++ js/common.js
@@ -1,28 +1,31 @@
-$(function() {
 
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
+$(document).ready(function() {
 
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
+// ADD CSS CLASS TO toggle_mnu
+
+	$(".top_mnu ul li a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
+	$(".menu-button").click(function() {
+		$(this).toggleClass('open');
+		$(".sandwich").toggleClass("active");
 	});
 
+	$(".menu-button").click(function () {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+
+		} else {
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+
+
 });
+
+
